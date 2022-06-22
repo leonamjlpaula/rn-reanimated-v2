f98953111e2a5bede926525b3350fc5a50082f63
Leonam de Paula <leonamjlpaula@gmail.com>
2022-06-22 09:17:43 -0300

    feat: PanGesture

--- src/PanGesture/PanGesture.tsx
+++ src/PanGesture/PanGesture.tsx
@@ -1,6 +1,9 @@
 import { View, StyleSheet } from "react-native";
+import { PanGestureHandler, PanGestureHandlerGestureEvent } from "react-native-gesture-handler";
+import Animated, { useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withDecay } from "react-native-reanimated";
+import { clamp } from "react-native-redash";
 
-import { Card, Cards } from "../components";
+import { Card, Cards, CARD_HEIGHT, CARD_WIDTH } from "../components";
 
 const styles = StyleSheet.create({
   container: {
@@ -13,11 +16,53 @@ interface GestureProps {
   height: number;
 }
 
+type AnimatedGHContext = {
+  offsetX: number;
+  offsetY: number;
+};
+
+
 export const PanGesture = ({ width, height }: GestureProps) => {
-  console.log({ width, height });
+
+  const boundX = width - CARD_WIDTH;
+  const boundY = height - CARD_HEIGHT;
+
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+  const onGestureEvent = useAnimatedGestureHandler<
+  PanGestureHandlerGestureEvent,
+  AnimatedGHContext
+>({
+    onStart:(_, ctx) => {
+      ctx.offsetX = translateX.value
+      ctx.offsetY = translateY.value
+    },
+    onActive: (event, ctx) => {
+      translateX.value = clamp(ctx.offsetX + event.translationX, 0, boundX)
+      translateY.value = clamp(ctx.offsetY + event.translationY, 0, boundY)
+    },
+    onEnd: (event) => {
+      translateX.value = withDecay({velocity: event.velocityX, clamp: [0, boundX]})
+      translateY.value = withDecay({velocity: event.velocityY, clamp: [0, boundY]})
+    }
+  })
+
+  const style = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {translateX: translateX.value},
+        {translateY: translateY.value}
+      ]
+    }
+  })
+
   return (
     <View style={styles.container}>
-      <Card card={Cards.Card1} />
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View {...{style}}>
+          <Card card={Cards.Card1} />
+        </Animated.View>
+      </PanGestureHandler>
     </View>
   );
 };
