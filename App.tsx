1c5615f1eb9aee34ce9a4f4305cfd079307166fd
Leonam de Paula <leonamjlpaula@gmail.com>
2022-06-21 22:36:23 -0300

    chore: completed scaffold

--- App.tsx
+++ App.tsx
@@ -1,20 +1,129 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import type { Routes } from "./src/Routes";
+import { Examples } from "./src/Examples";
+import { HeartOfTheMatter } from "./src/HeartOfTheMatter";
+import { PanGesture } from "./src/PanGesture";
+// import { Transitions } from "./src/Transitions";
+// import { CircularSlider } from "./src/CircularSlider";
+// import { Graph } from "./src/Graph";
+// import { Worklets } from "./src/Worklets";
+// import { DragToSort } from "./src/DragToSort";
+// import { DynamicSpring } from "./src/DynamicSpring";
+// import { Animations } from "./src/Animations";
+import { Swiping, swipingAssets } from "./src/Swiping";
+// import { Bezier } from "./src/Bezier";
+// import { ShapeMorphing } from "./src/ShapeMorphing";
+// import { Accordion } from "./src/Accordion";
+import { LoadAssets } from "./src/components";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const assets = [...swipingAssets];
+const Stack = createStackNavigator<Routes>();
+const App = () => (
+  <LoadAssets assets={assets}>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Examples"
+        component={Examples}
+        options={{
+          title: "Learn Reanimated 2",
+        }}
+      />
+      <Stack.Screen
+        name="TheHeartOfTheMatter"
+        component={HeartOfTheMatter}
+        options={{
+          title: "The Heart of the Matter",
+        }}
+      />
+      {/* <Stack.Screen
+        name="Worklets"
+        component={Worklets}
+        options={{
+          title: "Worklets",
+        }}
+      /> */}
+      {/* <Stack.Screen
+        name="Animations"
+        component={Animations}
+        options={{
+          title: "Animations",
+        }}
+      /> */}
+      <Stack.Screen
+        name="PanGesture"
+        component={PanGesture}
+        options={{
+          title: "PanGesture",
+        }}
+      />
+      {/* <Stack.Screen
+        name="Transitions"
+        component={Transitions}
+        options={{
+          title: "Transitions",
+        }}
+      />
+      <Stack.Screen
+        name="CircularSlider"
+        component={CircularSlider}
+        options={{
+          title: "Circular Slider",
+        }}
+      />
+      <Stack.Screen
+        name="Graph"
+        component={Graph}
+        options={{
+          title: "Graph",
+        }}
+      /> */}
+      {/* <Stack.Screen
+        name="DynamicSpring"
+        component={DynamicSpring}
+        options={{
+          title: "Dynamic Spring",
+        }}
+      />
+      <Stack.Screen
+        name="DragToSort"
+        component={DragToSort}
+        options={{
+          title: "Drag to Sort",
+        }}
+      /> */}
+      <Stack.Screen
+        name="Swiping"
+        component={Swiping}
+        options={{
+          title: "Swiping",
+        }}
+      />
+      {/* <Stack.Screen
+        name="Bezier"
+        component={Bezier}
+        options={{
+          title: "Bézier",
+          gestureEnabled: false,
+        }}
+      />
+      <Stack.Screen
+        name="ShapeMorphing"
+        component={ShapeMorphing}
+        options={{
+          title: "Shape Morphing",
+        }}
+      />
+      <Stack.Screen
+        name="Accordion"
+        component={Accordion}
+        options={{
+          title: "Accordion",
+        }}
+      /> */}
+    </Stack.Navigator>
+  </LoadAssets>
+);
+
+// eslint-disable-next-line import/no-default-export
+export default App;
